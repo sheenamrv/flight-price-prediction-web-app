9e4c5941485e05b2b8e7f230abe340a076677511
Andrianna Wardill <AndriannaM17@gmail.com>
2026-03-28 19:53:18 -0400

    Print Model
    Prints out the model evaluation of the last saved pkl for both models

--- API/future/future_main.py
+++ API/future/future_main.py
@@ -1,5 +1,6 @@
 from future_collector import request_flights, save_flights
 from future_analytics import get_live_analytics, get_hist_analytics
+from machine_learning.test_evaluate import evaluate_models
 # from predict_price import predict_prices
 
 origin = input("Origin airport: ").upper()
@@ -8,11 +9,6 @@ departure_date = input("Departure date (YYYY-MM-DD): ")
 
 # Collect new API data
 flights = request_flights(origin, destination, departure_date)
-
-for flight in flights:
-    flight["origin"] = origin
-    flight["destination"] = destination
-    flight["departure_date"] = departure_date
 
 # Get Analytics of live data
 live_stats = get_live_analytics(flights)
@@ -40,3 +36,10 @@ print("Highest Price:", hist_stats["highest_price"])
 print("Average Price:", hist_stats["average_price"])
 print("Total Records:", hist_stats["total_records"])
 print("Filtered Records (Origin, Destination):", hist_stats["filtered_records"])
+
+print("---------")
+print("\nModel Evaluation")
+print("\nEvaluation done on most recent future_engineering.py, train_xgboost.py, and train_random_forest.py")
+
+evaluation_results = evaluate_models()
+print(evaluation_results.to_string(index=False))
