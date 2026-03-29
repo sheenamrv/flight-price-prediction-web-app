77d25d289a601a590628e4d3c378cf2d032af581
Abolarinwa Elegbede <lekan_elegbede@yahoo.com>
2026-03-29 18:27:23 -0400

    commit

--- flight_price_prediction_project/flights/models.py
+++ flight_price_prediction_project/flights/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from zoneinfo import ZoneInfo
-
-EST = ZoneInfo("America/New_York")
 
 
 class SearchRecord(models.Model):
@@ -12,9 +9,5 @@ class SearchRecord(models.Model):
     average_live_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     searched_at = models.DateTimeField(auto_now_add=True)
 
-    @property
-    def searched_at_est(self):
-        return self.searched_at.astimezone(EST)
-
     def __str__(self):
-        return f"{self.origin} → {self.destination} on {self.departure_date} (searched {self.searched_at_est:%Y-%m-%d %H:%M %Z})"
+        return f"{self.origin} → {self.destination} on {self.departure_date} (searched {self.searched_at:%Y-%m-%d %H:%M})"
